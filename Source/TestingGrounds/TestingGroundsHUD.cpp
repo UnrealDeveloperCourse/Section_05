9c930471eb71e70edd986e766a81d1ada75d6b46
Jonas <jawabiscuit@users.noreply.github.com>
2018-01-15 15:42:32 -0500

    Asset Reorganization

--- Source/TestingGrounds/TestingGroundsHUD.cpp
+++ Source/TestingGrounds/TestingGroundsHUD.cpp
@@ -10,7 +10,7 @@
 ATestingGroundsHUD::ATestingGroundsHUD()
 {
 	// Set the crosshair texture
-	static ConstructorHelpers::FObjectFinder<UTexture2D> CrosshairTexObj(TEXT("/Game/Static/Player/Character/Textures/FirstPersonCrosshair"));
+	static ConstructorHelpers::FObjectFinder<UTexture2D> CrosshairTexObj(TEXT("/Game/Static/Character/FP_Textures/FirstPersonCrosshair"));
 	CrosshairTex = CrosshairTexObj.Object;
 }
 
