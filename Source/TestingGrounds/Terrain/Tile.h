d78742252bde2e6cd58115c964684f28a26ad4e1
Jonas <jawabiscuit@users.noreply.github.com>
2018-01-26 20:10:47 -0500

    Spawning into Free Space & Randomizing Rotation and Scale

--- Source/TestingGrounds/Terrain/Tile.h
+++ Source/TestingGrounds/Terrain/Tile.h
@@ -16,7 +16,10 @@ public:
 	ATile();
 
 	UFUNCTION(BlueprintCallable, Category = Setup)
-	void PlaceActors(TSubclassOf<AActor> ToSpawn, int MinSpawn, int MaxSpawn);
+	void PlaceActors(TSubclassOf<AActor> ToSpawn,
+		int MinSpawn = 1, int MaxSpawn = 1,
+		float Radius = 500, float MinScale = 1.f,
+		float MaxScale = 1.f);
 
 protected:
 	// Called when the game starts or when spawned
@@ -27,6 +30,11 @@ public:
 	virtual void Tick(float DeltaTime) override;
 
 private:
-	bool CastSphere(FVector Location, float Radius);
+	bool FindEmptyLocation(FVector& OutLocation, float Radius);
+
+	void PlaceActor(TSubclassOf<AActor> ToSpawn, FVector SpawnPoint,
+		float Rotation, float Scale);
+
+	bool CanSpawnAtLocation(FVector Location, float Radius);
 	
 };
